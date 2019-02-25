f9b8b1145e269e27dbdf9dcda0d0838aee77abdb
garglakshay139 <47963811+garglakshay139@users.noreply.github.com>
2019-02-25 11:44:02 +0530

    Uploaded the Welcome ConsoleApplication

---
+++ ConsoleApplication20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication20
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            DummyCode dummyCode = new DummyCode();
+            dummyCode.CheckPalindrome(5);
+           
+        }
+
+    }
+
+    public class DummyCode
+    {
+        public int Name { get; set; }
+
+        public void CheckPalindrome(int i)
+        {
+            if (i > 7)
+            {
+                i = 9;
+            }
+            else
+            {
+                i = 0;
+            }
+        }
+    }
+
+
+}
---
+++ UnitTests2/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ConsoleApplication20;
+
+namespace UnitTests2
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestPositive()
+        {
+            DummyCode dummyCode = new DummyCode();
+            dummyCode.CheckPalindrome(5);
+
+          //  DummyCode dummyCode = new DummyCode();
+            dummyCode.CheckPalindrome(9);
+        }
+
+
+    }
+}
